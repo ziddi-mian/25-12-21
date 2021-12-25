d6627461963ec595af8466c1f5957a1cf115c66b
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-25 13:31:10 +0500

    js

--- script.js
+++ script.js
@@ -308,3 +308,17 @@ function startOfWeek(date)
 dt = new Date(); 
 
 console.log(startOfWeek(dt).toString());
+
+
+// Write a JavaScript function to get the month start date.
+
+function startOfMonth(date)
+  {
+     
+   return new Date(date.getFullYear(), date.getMonth(), 1);
+ 
+  }
+
+dt = new Date(); 
+
+console.log(startOfMonth(dt).toString());
