109d1979885cec6df54e4438d5c339ba7901001f
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-25 20:09:02 +0500

    js

--- script.js
+++ script.js
@@ -324,6 +324,29 @@ dt = new Date();
 console.log(startOfMonth(dt).toString());
 
 
+let num1=parseInt(prompt("Enter first number"))
+let num2=parseInt(prompt("Enter second number"))
+let oper=prompt("which operation you want to do(+,-,*,/)")
+switch (true) {
+    case (oper=='+'):
+        console.log(num1+"+"+num2+"="+ (num1+num2));
+        break;
+    case (oper=='-'):
+        console.log(num1+"-"+num2+"="+ (num1-num2));
+        break;
+    case (oper=='*'):
+        console.log(num1+"*"+num2+"="+ (num1*num2));
+        break;
+    case (oper=='/'):
+        console.log(num1+"/"+num2+"="+ (num1/num2));
+        break;
+
+    default:
+        console.log("Enter valid operation.");
+        break;
+}
+
+
 
 // Write a JavaScript function to get the month end date.  
 
